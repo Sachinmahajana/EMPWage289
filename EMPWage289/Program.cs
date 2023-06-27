ab67d0eda966f9ee94c39ba0b2318b9f3d3529b9
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-27 10:59:40 +0530

    [Sachin] Adding Welcome message

---
+++ EMPWage289/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EMPWage289
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the EmpWage Program");
+        }
+    }
+}
